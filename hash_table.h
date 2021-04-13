6351fa4ecf15c34680ef83b87abf32ac11def1f4
Michel de Muret de Labouret <michel.de-muret-de-labouret@etu.u-bordeaux.fr>
2021-04-13 20:00:16 +0200

    structures hash_table et coordinate

---
+++ hash_table.h
@@ -0,0 +1,50 @@
+/**
+ * @file hash_table.h
+ * @brief Hash table structure.
+ * @details See @ref index for further details.
+ **/
+
+#ifndef __HASH_TABLE_H__
+#define __HASH_TABLE_H__
+
+typedef struct
+{
+    char *indice;
+    void *data;
+} cell_s;
+
+typedef struct hach
+{
+    cell_s *p_cell;
+    int size;
+} hach_s;
+
+/**
+ * @brief Creates a new empty hash table.
+ * @return an empty hash table
+ **/
+hach_s *hach_new(void);
+
+/**
+ * @brief Add or update in the hash table the height of the key.
+ * @param p_table the hash table
+ * @param key the coordinate
+ * @param data the height 
+ **/
+void hach_add(hach_s *p_table, const char *key, void *data);
+
+/**
+ * @brief Find in the hash table the height of the key.
+ * @param p_table the hash table
+ * @param key the coordinate
+ * @return the height of the coordinate if it find it
+ **/
+void *hach_search(hach_s *p_table, const char *key);
+
+/**
+ * @brief Deletes the hash table and frees the allocated memory.
+ * @param p_table the hash table to delete
+ **/
+void hach_delete(hach_s **p_table)
+
+#endif  // __HASH_TABLE_H__
--- tools.h
+++ tools.h
@@ -1,3 +1,35 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "hash_table.h"
+
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+/**
+ * @brief Different direction used to define an edge of a map
+ **/
+typedef enum {
+    NORTH = 0,
+    EST = 1,
+    SOUTH = 2,
+    WEST = 3,
+} direction;
+
+/**
+ * @brief Different end result for the fonction fill_map_edge_from_direction_list
+ **/
+typedef enum {
+    SHAPE_IS_PAVABLE = 0,
+    SHAPE_IS_NOT_PAVABLE = 1;
+    SHAPE_IS_DICONECTED = 2,
+    ERROR_DIRECTION_TABLE = 3;
+    ERROR_MAP = 4;
+} how_fill_fonction_worked;
+
+
+how_fill_fonction_worked fill_map_edge_from_direction_list(direction[] list_of_direction; hach_s map_of_height);
+
+
+#endif  // __TOOLS_H__
