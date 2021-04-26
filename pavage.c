ddc9771cdc972c8c046d7d3bebb596d177968f15
Raphael Chavagnac <raphael.chavagnac@etu.u-bordeaux.fr>
2021-04-26 17:46:50 +0200

    x/y min/max

--- pavage.c
+++ pavage.c
@@ -3,3 +3,43 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int Xmin(hash_s *hash) {
+    int min = 0;
+    for (uint i = 0; i < hash->size; i++) {
+        if (hash->p_cell[i]->key->x < min) {
+            min = hash->p_cell[i]->key->x;
+        }
+    }
+    return min;
+}
+
+int Xmax(hash_s *hash) {
+    int max = 0;
+    for (uint i = 0; i < hash->size; i++) {
+        if (hash->p_cell[i]->key->x > max) {
+            max = hash->p_cell[i]->key->x;
+        }
+    }
+    return max;
+}
+
+int Ymin(hash_s *hash) {
+    int min = 0;
+    for (uint i = 0; i < hash->size; i++) {
+        if (hash->p_cell[i]->key->y < min) {
+            min = hash->p_cell[i]->key->y;
+        }
+    }
+    return min;
+}
+
+int Ymax(hash_s *hash) {
+    int max = 0;
+    for (uint i = 0; i < hash->size; i++) {
+        if (hash->p_cell[i]->key->y > max) {
+            max = hash->p_cell[i]->key->y;
+        }
+    }
+    return max;
+}
--- pavage.h
+++ pavage.h
@@ -24,8 +24,34 @@ typedef enum {
     ERROR_MAP = 4,
 } how_fill_fonction_worked;
 
+how_fill_fonction_worked fill_map_edge_from_direction_list(direction list_of_direction[], hash_s *map_of_height);
 
-how_fill_fonction_worked fill_map_edge_from_direction_list(direction list_of_direction[], hash_s* map_of_height);
+/**
+ * @param hash a pointer on a hash table
+ * @return the smallest abscissas of a given hash table
+ * @pre @p hash must be a valid pointer toward a hash_s structure
+ * */
+int Xmin(hash_s *hash);
 
+/**
+ * @param hash a pointer on a hash table
+ * @return the highest abscissas of a given hash table
+ * @pre @p hash must be a valid pointer toward a hash_s structure
+ * */
+int Xmax(hash_s *hash);
+
+/**
+ * @param hash a pointer on a hash table
+ * @return the smallest ordinates of a given hash table
+ * @pre @p hash must be a valid pointer toward a hash_s structure
+ * */
+int Ymin(hash_s *hash);
+
+/**
+ * @param hash a pointer on a hash table
+ * @return the highest ordinates of a given hash table
+ * @pre @p hash must be a valid pointer toward a hash_s structure
+ * */
+int Ymax(hash_s *hash);
 
 #endif  // __TOOLS_H__
