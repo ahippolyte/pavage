09d3852ebeba4a96963b84471acb3c3bf8ae164d
Michel de Muret de Labouret <michel.de-muret-de-labouret@etu.u-bordeaux.fr>
2021-04-26 19:02:37 +0200

    creation fonction create edge from direction list

--- pavage.c
+++ pavage.c
@@ -1,5 +1,56 @@
 #include "pavage.h"
 
+
+
+
+how_fill_fonction_worked fill_map_edge_from_direction_list(queue_s *list_of_direction, hash_s* map_of_height){
+
+    hash_add(map_of_height,coordinate_new(0,0),0);
+
+    return fill_map_edge_from_direction_list_recur(list_of_direction[], map_of_height, coordinate_new(0,0));
+}
+
+
+
+fill_map_edge_from_direction_list_recur(queue_s *list_of_direction, hash_s* map_of_height, coordinate last_coordinate){
+    if (list_of_direction == NULL){
+        return ERROR_DIRECTION_TABLE;
+    }
+
+    if (map_of_height == NULL){
+        return ERROR_MAP;
+    }
+
+    if (queue_is_empty(list_of_direction)){
+        if (coordinate_compare(last_coordinate,coordinate_new(0,0))){
+            return SHAPE_IS_MAYBE_PAVABLE;
+        }
+        else{
+            return SHAPE_IS_DICONECTED;
+        }
+    }
+
+    direction new_direction = queue_peek(list_of_direction);
+    queue_dequeue(list_of_direction);
+    coordinate new_coordinate = next_coordinate(last_coordinate,new_direction);
+
+    if (coordinate_compare(new_coordinate,coordinate_new(0,0)) && queue_is_empty(list_of_direction)){
+        if (calculate_height(last_coordinate, hash_search(map_of_height ,old_coordinate),new_direction) == 0 ){
+            return SHAPE_IS_MAYBE_PAVABLE;
+        }else{
+            return SHAPE_IS_NOT_PAVABLE;
+        }
+    }
+
+    if (hash_search(map_of_height ,new_coordinate) != INT_MAX){
+        return SHAPE_IS_DICONECTED;
+    }
+
+    hash_add(map_of_height,new_coordinate,calculate_height(last_coordinate, hash_search(map_of_height ,old_coordinate),new_direction));
+
+    return fill_map_edge_from_direction_list_recur(list_of_direction, map_of_height, new_coordinate);
+}
+
 int Xmin(hash_s *hash) {
     int min = 0;
     for (uint i = 0; i < hash->size; i++) {
