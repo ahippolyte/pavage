4f41baef2fa9f4f73757b0007e11bc0d0ca5d33a
Angel Hippolyte <angel.hippolyte@etu.u-bordeaux.fr>
2021-04-26 18:30:00 +0200

    Ajout d'une file

---
+++ queue.c
@@ -0,0 +1,111 @@
+#include "pavage_private.h"
+#include "queue.h"
+
+queue_s * queue_new(uint size){
+    queue_s * queue = (queue_s*)malloc(sizeof(queue_s));
+    if (queue == NULL) {
+        fprintf(stderr, "Allocation failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    queue->items = (int*)malloc(size*sizeof(int));
+    if (queue->items == NULL) {
+        fprintf(stderr, "Allocation failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    queue->size = size;
+    queue->front = -1;
+    queue->rear = -1;
+
+    return queue;
+}
+
+void queue_print(queue_s * queue) {
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+
+    printf(" _________________\n");
+    printf("|_____ QUEUE _____|\n\n");
+    printf("Size: %u\n", queue->size);
+    printf("________________________\n\n");
+    for (uint i = 0; i < queue->size; i++) {
+        printf("ELEMENT %u\n", i);
+        printf("Coordinates: ");
+        printf("%d ", queue->items[i]);
+        printf("---------\n");
+    }
+    printf(" _______________________ \n");
+    printf("|_________ END _________|\n");
+    return;
+}
+
+void queue_enqueue(queue_s * queue, int item){
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+
+    if (queue->rear == queue->size-1)
+        printf("Queue is full!\n");
+    else {
+        if (front == -1)
+            front = 0;
+        queue->rear++;
+        queue->items[queue->rear] = item;
+        printf("Inserted -> %d\n", item);
+    }
+}
+
+void queue_dequeue(queue_s * queue){
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+
+    if (queue->front == -1)
+        printf("Queue is empty!\n");
+    else {
+        printf("Deleted : %d\n", queue->items[queue->front]);
+        queue->front++;
+        if (queue->front > queue->rear)
+            queue->front = queue->rear = -1;
+    }
+}
+
+bool queue_is_empty(queue_s * queue){
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+
+    if(queue->size = 0){
+        return true;
+    }
+    return false;
+}
+
+int queue_peek(queue_s * queue){
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+
+    if(queue_is_empty(queue))
+        printf("Can't peek into an empty queue!");
+    else
+        return queue->items[queue->rear];
+}
+
+int queue_delete(queue_s * queue){
+    if (queue == NULL) {
+        fprintf(stderr, "Invalid pointer reference!");
+        exit(EXIT_FAILURE);
+    }
+    
+    free(queue->items);
+    free(queue);
+}
+
---
+++ queue.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+/**
+ * @brief Different direction used to define an edge of a map
+ **/
+typedef enum {
+    NORTH = 0,
+    EST = 1,
+    SOUTH = 2,
+    WEST = 3,
+} direction;
+
+#define SIZE 5
+
+typedef struct queue{
+    int items[];
+    uint size;
+    int front, rear;
+} queue_s;
+
+queue_s * queue_new();
+void queue_print(queue_s * queue);
+void queue_enqueue(queue_s * queue, int item);
+void queue_dequeue(queue_s * queue);
+bool queue_is_empty(queue_s * queue);
+int queue_peek(queue_s * queue);
+void queue_delete(queue_s * queue);
+
+
+int items[SIZE], front = -1, rear = -1;
